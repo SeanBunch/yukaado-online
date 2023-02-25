f646e9ee5cda6a521b2880a60f263e8fa728e59b
Sean Bunch <100799798+SeanBunch@users.noreply.github.com>
2023-02-24 20:02:38 -0500

    Hello world

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="./bosskeyCEO.PNG" className="App-logo" alt="Boss" />
+        <p>
+          BossKey Inc.
+        </p>
+        <a
+          className="App-link"
+          href="https://youtu.be/D_Vg4uyYwEk?t=57"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn About what BossKey can do for you. Click this link and get Motivated!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
